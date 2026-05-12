25ff4f02b568bf4310ac921ab7a7e16f846490db
JoonsangLee <joonsang0328@gmail.com>
2026-05-13 01:12:51 +0900

    delete and remake  multipleInheritence project

--- design-pattern/multipleInheritence/multipleInheritence/src/multipleInheritence/Bank.java
+++
@@ -1,7 +0,0 @@
-package multipleInheritence;
-
-public interface Bank {
-	public void withdraw(); // 출금 메서드  
-}
-
-//---------------------------------------------------------------------------------------
